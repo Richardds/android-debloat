57ac50d493dff0c2185cb983262c3b9e5067fbe3
Richard Boldiš <richard@boldis.dev>
2022-08-14 19:13:42 +0200

    Tweak bash vars

--- aapt.sh
+++ aapt.sh
@@ -7,7 +7,7 @@
 AAPT_WSL_PATH=/mnt/c/Development/Android/build-tools/33.0.0/aapt.exe
 
 if [ "$(uname -a | grep microsoft-standard-WSL)" ]; then
-    "${AAPT_WSL_PATH}" $@ | dos2unix
+    "${AAPT_WSL_PATH}" ${@} | dos2unix
 else
-    aapt $@
+    aapt ${@}
 fi
--- adb.sh
+++ adb.sh
@@ -7,7 +7,7 @@
 ADB_WSL_PATH=/mnt/c/Development/Android/platform-tools/adb.exe
 
 if [ "$(uname -a | grep microsoft-standard-WSL)" ]; then
-    "${ADB_WSL_PATH}" $@ | dos2unix
+    "${ADB_WSL_PATH}" ${@} | dos2unix
 else
-    adb $@
+    adb ${@}
 fi
